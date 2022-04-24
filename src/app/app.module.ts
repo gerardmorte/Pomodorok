993d86f22b8f071c508ca6d7a68317aeffd069fb
Gerard Morte <gerardmorte@gmail.com>
2022-04-24 03:48:12 +0200

    add navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { CountDownComponent } from './count-down/count-down.component';
@@ -7,16 +8,23 @@ import { FormComponent } from './form/form.component';
 import { StatisticsComponent } from './statistics/statistics.component';
 import { SettingsComponent } from './settings/settings.component';
 
+const appRoutes: Routes = [
+  { path: '', component: CountDownComponent },
+  { path: 'statistics', component: SettingsComponent },
+  { path: 'settings', component: StatisticsComponent },
+];
+
 @NgModule({
   declarations: [
     AppComponent,
     CountDownComponent,
     FormComponent,
     StatisticsComponent,
-    SettingsComponent
+    SettingsComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
