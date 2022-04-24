568db6d67a5e44d454b6dac1d6aa03aa0dca0354
Gerard Morte <gerardmorte@gmail.com>
2022-04-24 03:01:50 +0200

    add statistics and settings components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CountDownComponent } from './count-down/count-down.component';
 import { FormComponent } from './form/form.component';
+import { StatisticsComponent } from './statistics/statistics.component';
+import { SettingsComponent } from './settings/settings.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CountDownComponent,
-    FormComponent
+    FormComponent,
+    StatisticsComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule
