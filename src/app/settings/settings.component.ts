8a55f64366435fc3ff7a47cd7a4ccbf6c7b55328
gmorte <82134542+gmorte@users.noreply.github.com>
2022-04-27 11:02:01 +0200

    delete statistics from settings component works

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -11,28 +11,59 @@ export class SettingsComponent implements OnInit {
   @ViewChild('range2') range2: ElementRef;
   @ViewChild('range3') range3: ElementRef;
 
-  range1Value: number = 25;
-  range2Value: number = 5;
-  range3Value: number = 15;
+  settings: any = [];
+
+  range1Value: number = this.settings[0];
+  range2Value: number = this.settings[1];
+  range3Value: number = this.settings[2];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.getSettings();
+    console.log(this.getSettings);
+    this.range1Value = this.settings[0];
+    this.range2Value = this.settings[1];
+    this.range3Value = this.settings[2];
   }
 
-  ngAfterViewInit() {
-  }
-
-  getValueFromRange1(){
+  getValueFromRange1() {
     this.range1Value = this.range1.nativeElement.value;
   }
 
-  getValueFromRange2(){
+  getValueFromRange2() {
     this.range2Value = this.range2.nativeElement.value;
   }
 
-  getValueFromRange3(){
+  getValueFromRange3() {
     this.range3Value = this.range3.nativeElement.value;
+  }
+
+  localStorageSettings(array: any) {
+    localStorage.setItem("localSettings", JSON.stringify(array));
+  }
+
+  getSettings() {
+    var storedList = localStorage.getItem("localSettings");
+    if (storedList == null) {
+      let storedList = [];
+    } else {
+      this.settings = JSON.parse(storedList);
+    }
+    return this.settings;
+  }
+
+  saveSettings() {
+    this.settings = [this.range1Value, this.range2Value, this.range3Value]
+    this.localStorageSettings(this.settings);
+  }
+
+  deleteStatistics(){
+    this.localStorageEstadisticas([]);
+  }
+
+  localStorageEstadisticas(array: any) {
+    localStorage.setItem("localEstadisticas", JSON.stringify(array));
   }
 
 }
