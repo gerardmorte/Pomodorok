42ff19bab606d4019ef93345029d4db59a51d6ee
gmorte <82134542+gmorte@users.noreply.github.com>
2022-04-27 10:41:25 +0200

    input range settings works

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-settings',
@@ -7,9 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsComponent implements OnInit {
 
+  @ViewChild('range1') range1: ElementRef;
+  @ViewChild('range2') range2: ElementRef;
+  @ViewChild('range3') range3: ElementRef;
+
+  range1Value: number = 25;
+  range2Value: number = 5;
+  range3Value: number = 15;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit() {
+  }
+
+  getValueFromRange1(){
+    this.range1Value = this.range1.nativeElement.value;
+  }
+
+  getValueFromRange2(){
+    this.range2Value = this.range2.nativeElement.value;
+  }
+
+  getValueFromRange3(){
+    this.range3Value = this.range3.nativeElement.value;
   }
 
 }
