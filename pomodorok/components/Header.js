54b7734f3b3a53c831e853bdd1d036d873fd0a76
Gerard Morte <gerardmorte@gmail.com>
2022-12-18 18:48:17 +0100

    feat: Add hamburger-react component to navbar

--- pomodorok/components/Header.js
+++ pomodorok/components/Header.js
@@ -1,30 +1,24 @@
 import React from "react";
 import { useState } from "react";
+import Hamburger from "hamburger-react";
 
 export default function Header() {
-  const [menuOpen, setMenuOpen] = useState("hidden");
-
-  function handleClick() {
-    if (menuOpen === "hidden") {
-      setMenuOpen("visible");
-    }
-    if (menuOpen === "visible") {
-      setMenuOpen("hidden");
-    }
-  }
+  const [isOpen, setOpen] = useState(false);
 
   return (
     <>
       <div>
-        <div className="w-100 bg-slate-800 p-5 md:flex items-baseline justify-around">
-          <button
-            className="text-red-700 text-2xl md:hidden absolute right-0 mr-4 mt-1"
-            onClick={handleClick}
+        <div className="w-100 bg-slate-800 p-5 md:flex items-center justify-around">
+          <div className="md:hidden absolute right-0 mr-4">
+            <Hamburger toggled={isOpen} toggle={setOpen} />
+          </div>
+
+          <h1 className="text-3xl text-white font-bold mt-1">Pomodorok</h1>
+          <ul
+            className={`md:flex gap-10 text-2xl text-white ${
+              isOpen === false ? "hidden" : "visible"
+            }`}
           >
-            X
-          </button>
-          <h1 className="text-3xl text-white font-bold">Pomodorok</h1>
-          <ul className={`md:flex gap-10 text-2xl text-white mt-4 ${menuOpen}`}>
             <li className="mt-2">Home</li>
             <li className="mt-2">Statistics</li>
             <li className="mt-2">Settings</li>
